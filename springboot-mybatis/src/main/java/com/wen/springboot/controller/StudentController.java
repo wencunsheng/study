e9410b4e9f704766d8737bb2a068c18c0d75ec29
laocunsheng <laocunsheng@163.com>
2018-08-15 18:02:31 +0800

    添加log4j日志打印, 没法打印mybatis SQL有点烦

--- springboot-mybatis/src/main/java/com/wen/springboot/controller/StudentController.java
+++ springboot-mybatis/src/main/java/com/wen/springboot/controller/StudentController.java
@@ -2,6 +2,8 @@ package com.wen.springboot.controller;
 
 import com.wen.springboot.domain.Student;
 import com.wen.springboot.service.IStudentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,17 +13,20 @@ import javax.annotation.Resource;
 public class StudentController
 {
     @Resource
-    IStudentService studentService;
+    private IStudentService studentService;
+    private Logger l = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping(value = "/index")
     public String index()
     {
+        l.info("666666666666666666666666666666666");
         Student student = new Student();
         student.setAge("18");
         student.setName("12");
         studentService.addStudent(student);
         return "ok";
     }
+
     @RequestMapping("/hello")
     public String hello()
     {
--- springboot-mybatis/src/main/java/com/wen/springboot/service/impl/StudentServiceImpl.java
+++ springboot-mybatis/src/main/java/com/wen/springboot/service/impl/StudentServiceImpl.java
@@ -4,17 +4,16 @@ import com.wen.springboot.domain.Student;
 import com.wen.springboot.mapper.StudentMapper;
 import com.wen.springboot.service.IStudentService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
 @Service
-public class StudentServiceImpl implements IStudentService
-{
+public class StudentServiceImpl implements IStudentService {
     @Resource
     private StudentMapper studentMapper;
-
-    public void addStudent(Student student)
-    {
+    @Transactional
+    public void addStudent(Student student) {
         studentMapper.add(student);
     }
 }
